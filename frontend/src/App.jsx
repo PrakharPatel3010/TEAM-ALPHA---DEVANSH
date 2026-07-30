d15ae321eaa663cfa12b5e0586da4d88444450e5
PrakharPatel3010 <prakharpatel67@gmail.com>
2026-07-30 17:55:25 +0530

    Initial project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Research Paper Briefing Agent</h1>
+
+      <input type="file" accept=".pdf" />
+
+      <br /><br />
+
+      <button>Upload PDF</button>
+    </div>
+  );
+}
+
+export default App;

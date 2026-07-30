7013c9661c9d8c31acd6bedde73d5650fd1320a5
PrakharPatel3010 <prakharpatel67@gmail.com>
2026-07-30 21:34:51 +0530

    Extract Text From Uploaded PDF

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
+import os
+import shutil
+import fitz
 
 app = FastAPI()
 
@@ -13,6 +16,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def extract_text_from_pdf(file_path):
+    text = ""
+
+    pdf = fitz.open(file_path)
+
+    for page in pdf:
+        text += page.get_text()
+
+    pdf.close()
+
+    return text
+
 @app.get("/")
 def home():
     
@@ -20,6 +35,20 @@ def home():
     # Upload endpoint
 @app.post("/upload")
 async def upload_pdf(file: UploadFile = File(...)):
+
+    upload_folder = "uploads"
+
+    os.makedirs(upload_folder, exist_ok=True)
+
+    file_path = os.path.join(upload_folder, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    text = extract_text_from_pdf(file_path)
+
     return {
-        "filename": file.filename
+        "message": "Upload successful",
+        "filename": file.filename,
+        "text": text
     }

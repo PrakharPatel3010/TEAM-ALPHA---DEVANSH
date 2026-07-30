0cc649661c96aca58ddff6b2618f98cbe19be9d5
PrakharPatel3010 <prakharpatel67@gmail.com>
2026-07-31 01:31:57 +0530

    adding help functions in Chroma

--- backend/database/chroma_db.py
+++ backend/database/chroma_db.py
@@ -1,9 +1,26 @@
 import chromadb
 
-# Create a persistent database
 client = chromadb.PersistentClient(path="./chroma_db")
 
-# Create (or get) a collection
 collection = client.get_or_create_collection(
     name="research_papers"
 )
+
+def add_chunk(chunk_id, text, embedding, metadata=None):
+    collection.add(
+        ids=[chunk_id],
+        documents=[text],
+        embeddings=[embedding],
+        metadatas=[metadata or {}]
+    )
+
+def search_chunks(query_embedding, n_results=3):
+    return collection.query(
+        query_embeddings=[query_embedding],
+        n_results=n_results
+    )
+
+def clear_collection():
+    collection.delete(
+        ids=collection.get()["ids"]
+    )

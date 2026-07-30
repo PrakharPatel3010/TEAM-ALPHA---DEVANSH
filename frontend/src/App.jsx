1181ee202301d0d4e9f4a3d51e54108c13134144
PrakharPatel3010 <prakharpatel67@gmail.com>
2026-07-30 19:00:47 +0530

    Connect React Front end to Back end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/")
+      .then((response) => response.json())
+      .then((data) => {
+        setMessage(data.message);
+      })
+      .catch((error) => {
+        console.error(error);
+        setMessage("Could not connect to backend");
+      });
+  }, []);
+
   return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+    <div style={{ padding: "30px" }}>
       <h1>Research Paper Briefing Agent</h1>
 
-      <input type="file" accept=".pdf" />
+      <h3>Backend Status:</h3>
 
-      <br /><br />
-
-      <button>Upload PDF</button>
+      <p>{message}</p>
     </div>
   );
 }
